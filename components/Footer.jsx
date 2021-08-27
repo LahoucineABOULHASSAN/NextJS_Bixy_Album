83eb94f1e389da8bff4da44197d58c5509529147
lahoucine aboulhassan <em.aboulhassan@gmail.com>
2021-08-27 17:39:10 +0100

    init app + header and footer

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="flex flex-col items-center justify-center  bg-gray-300">
+      <h1 className="text-3xl w-full text-center py-4 my-8">
+        &copy; BixyAlbum
+      </h1>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="flex flex-col items-center justify-center  bg-gray-300">
+      <h1 className="text-3xl w-full text-center py-4 my-8">
+        Welcome to BixyAlbum
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
